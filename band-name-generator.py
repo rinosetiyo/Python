acb82c0281627a7184b066a28144fe3a37dc773d
rinosetiyo <42023075+rinosetiyo@users.noreply.github.com>
2022-10-07 11:09:18 +0700

    make first python project

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print("welcome to band name generator")
+name = input("where is your born and grew up ?\n")
+pet_name = input("who is your pet's name ?\n")
+print("your band name " + name +" "+ pet_name)
